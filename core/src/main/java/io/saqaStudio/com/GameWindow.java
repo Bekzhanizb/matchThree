e4eb6eb2da2537c5d78f3eaba56562c9218cecee
Bekezhan <bekzhanizbasar228@gmail.com>
2025-05-16 17:17:18 +0500

    Updated GameWindow

--- core/src/main/java/io/saqaStudio/com/GameWindow.java
+++ core/src/main/java/io/saqaStudio/com/GameWindow.java
@@ -1,9 +1,6 @@
 package main.java.io.saqaStudio.com;
 
-import com.badlogic.gdx.scenes.scene2d.ui.Button;
-import com.badlogic.gdx.scenes.scene2d.ui.Image;
-import com.badlogic.gdx.scenes.scene2d.ui.Skin;
-import com.badlogic.gdx.scenes.scene2d.ui.Window;
+import com.badlogic.gdx.scenes.scene2d.ui.*;
 
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Window;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Graphics;
@@ -12,6 +9,21 @@ public class GameWindow extends Window {
 
     public GameWindow(String title, Skin skin) {
         super(title, skin);
+
+        getTitleTable().clearChildren();
+        getTitleTable().defaults().space(5.0f);
+
+        Button button = new Button(skin, "Close");
+
+        getTitleTable().add(button);
+
+        Image image = new Image(skin, "title-bar-bg");
+        image.setScale(0.5f);
+        getTitleTable().add(image).growX();
+
+        Label label = new Label("Math Three Game", skin);
+        label.setFontScale(1.15f);
+        getTitleTable().add(label).growX();
     }
 
     public GameWindow(String title, Skin skin, String styleName) {
