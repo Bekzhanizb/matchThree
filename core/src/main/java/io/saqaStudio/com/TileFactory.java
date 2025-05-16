d927f2863628e4020dcf345a687dd545635721f4
Bekezhan <bekzhanizbasar228@gmail.com>
2025-05-16 22:10:30 +0500

    Concreted TileFactory Design Pattern

---
+++ core/src/main/java/io/saqaStudio/com/TileFactory.java
@@ -0,0 +1,17 @@
+package main.java.io.saqaStudio.com;
+
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.utils.Array;
+
+public class TileFactory {
+    public static Tile creteRandomTile(Array<TextureAtlas.AtlasRegion> entities,
+                                       ClickListener clickListener) {
+        int index = MathUtils.random(0, entities.size - 1);
+        Tile tile = new Tile();
+        tile.addListener(clickListener);
+        tile.inti(entities.get(index), index);
+        return tile;
+    }
+}
