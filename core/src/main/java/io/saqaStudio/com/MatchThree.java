b6b23d69f7550261bf2fb205f6d6b45ec13429b8
Bekezhan <bekzhanizbasar228@gmail.com>
2025-05-16 16:52:50 +0500

    Initial Commit

---
+++ core/src/main/java/io/saqaStudio/com/MatchThree.java
@@ -0,0 +1,11 @@
+package io.saqaStudio.com;
+
+import com.badlogic.gdx.Game;
+
+/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
+public class MatchThree extends Game {
+    @Override
+    public void create() {
+        setScreen(new FirstScreen());
+    }
+}
